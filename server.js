8e773990a477fc1a9c4b1118e364e747f34f75da
hassan <asd@MacBook-Air-von-asd.local>
2024-08-07 13:39:29 +0500

    feature: chat-endpoint, create endpoint to interact with locally running llm

--- server.js
+++ server.js
@@ -16,6 +16,31 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.post("/chat", async (req, res) => {
+  const placerholderQuery = 'just say "Hello, how can I help you today?"';
+  const query = req?.body?.query || placerholderQuery;
+
+  try {
+    const response = await axios.post(MODEL_URL, createOllamaQuery(query));
+    const {
+      message: { content },
+    } = response?.data;
+
+    res.json({ content });
+  } catch (error) {
+    console.log("Error fetching model response, full error - ", error);
+    res.status(500).json({ error: "Error fetching model response" });
+  }
+});
+
+function createOllamaQuery(input) {
+  return {
+    model: "dolphin-llama3",
+    messages: [{ role: "user", content: `${input}` }],
+    stream: false,
+  };
+}
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
